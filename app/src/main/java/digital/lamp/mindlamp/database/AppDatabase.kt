abb50929b604550a15f830643c74fe237db3a3a8
amaljofy10 <amaljofy10@gmail.com>
2021-02-16 09:34:52 +0530

    Sensor spec push

--- app/src/main/java/digital/lamp/mindlamp/database/AppDatabase.kt
+++ app/src/main/java/digital/lamp/mindlamp/database/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import digital.lamp.mindlamp.utils.SingletonHolder
 
-@Database(entities = [Analytics::class, SensorSpecs::class], version = 1, exportSchema = false)
+@Database(entities = [Analytics::class, SensorSpecs::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun analyticsDao(): AnalyticsDao
     abstract fun sensorDao(): SensorDao
